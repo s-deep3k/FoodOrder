cd1a1eed1d67af059c6d799ae5d1b5788a5587d6
Shankhadeep Biswas <sankhadeep.genx@gmail.com>
2021-09-04 03:35:41 +0530

    Button modifieed

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react"
 import classes from "./Header.module.css"
 import mealImage from "../../assets/meals.jpg"
+import HeaderCart from "./HeaderCart"
 
 const Header = props =>{
     return(
         <React.Fragment>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <button>Cart</button>
+                <HeaderCart></HeaderCart>
             </header>
             <div className ={classes["main-image"]}>
             <img src={mealImage} alt="A table full of very delicious food!"></img> 
